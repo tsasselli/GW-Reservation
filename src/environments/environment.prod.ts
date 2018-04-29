5cf442a3737b03ca41e0c159446e90f8d79738f2
tsasselli <tsasselli@gmail.com>
2018-04-29 16:57:31 -0700

    deployed to firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBHjhfb2cRZhZlDPOftpMcSB74_P8dNVpk",
+    authDomain: "gw-reservations.firebaseapp.com",
+    databaseURL: "https://gw-reservations.firebaseio.com",
+    projectId: "gw-reservations",
+    storageBucket: "",
+    messagingSenderId: "211027746285"
+  }
 };
